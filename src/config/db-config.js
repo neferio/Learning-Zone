7943c4cc24c645e94e01e1837a214addba2f7c78
Siddharth kandola <siddharth.kandola@gmail.com>
2020-10-28 13:27:18 +0530

    some extra changes for aws

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,9 +1,13 @@
 module.exports = {
    
-       'host':'localhost',
-       'user':'root',
-       'password':'',
-       'port':'3306',
+    //    'host':'localhost',
+    //    'user':'root',
+    //    'password':'',
+    //    'port':'3306',
+    'host':'aa104mz4lxj92s4.cxb0soi82sf9.us-east-2.rds.amazonaws.com',
+    'user':'localhost',
+    'passoword':'siddharth',
+    'port':'3306',
     
    
    'database':'students',
