0b577b0c833aee07a364819000ac235f1c78b72f
Meetanshi Mittal <meetanshi.18@gmail.com>
2020-11-21 18:15:53 +0530

    restructure pt. 2

--- web/src/app.js
+++ web/src/app.js
@@ -2,6 +2,7 @@ const express=require('express')
 const path=require('path')
 
 const port=process.env.PORT || 3000
+const app = express();
 
 app.listen(port,()=>{
     console.log('server is running on',port)
