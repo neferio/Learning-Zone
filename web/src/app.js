329a373e51f82853ee781ff8d7ca044b652227d1
Meetanshi Mittal <meetanshi.18@gmail.com>
2020-11-24 22:27:58 +0530

    some paths corrected

--- web/src/app.js
+++ web/src/app.js
@@ -1,10 +1,43 @@
 const express=require('express')
 const path=require('path')
 
-const port = process.env.PORT || 4000
-const app = express();
+const port=process.env.PORT || 4000
+
+const htmlpath=path.join(__dirname,'../public')
+
+
+
+//Express Server
+const app=express()
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(htmlpath))
+app.set("view engine", "ejs");
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.get('/signup',(req,res)=>{
+    res.render("signup");
+})
+
+app.get('/login',(req,res)=>{
+    res.render("login");
+})
+
+
+
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log('server is running on',port)
 })
-
