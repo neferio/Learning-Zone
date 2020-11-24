1d598f565419ccc6b7498941ff2db5b3ba1adf48
Meetanshi Mittal <32373124+Meetanshi18@users.noreply.github.com>
2020-11-24 16:10:09 +0530

    Update app.js

--- web/src/app.js
+++ web/src/app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const path=require('path')
 
-const port=process.env.PORT || 3000
+const port=process.env.PORT || 4000
 const app = express();
 
 app.listen(port,()=>{
