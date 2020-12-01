0dcb94eec057e7998a26ea47278e834779fe93b5
neferio <51848315+neferio@users.noreply.github.com>
2020-12-01 15:55:00 +0530

    bug fix

--- web/src/db/database.js
+++ web/src/db/database.js
@@ -4,19 +4,14 @@ const db_Config = require("../config/db-config.js");
 // Create a connection to database
 
 const conn = mysql.createConnection({
-<<<<<<< HEAD
-  // host: 'db',
-=======
+
   host: 'db',
   user: 'mysql',
   password: '1234',
-  // host: 'localhost',
->>>>>>> 6f900428050df59af3b09514cfdc522cf3197661
-  // user: 'root',
-  // password: '1234',
-  host: 'localhost',
-  user: 'root',
-  password: '',
+
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
   database: db_Config.database,
   port:db_Config.port
 });
